f2498c3dd32a4f149ce7138af07468421902deb8
Debjit Pramanik <79047521+debjit20504@users.noreply.github.com>
2021-08-10 11:39:11 +0530

    Update binary.py

--- Simple-Assembler/binary.py
+++ Simple-Assembler/binary.py
@@ -2,7 +2,17 @@ def addition():
 	reg2 = int(input())
 	reg3 = int(input())
 	reg1 = reg2 + reg3
-	print('00000'+'00'+bin(reg1)+bin(reg2)+bin(reg3))
+# 	print('00000'+'00'+bin(reg1)+bin(reg2)+bin(reg3))
+	
+def subtraction():
+	reg2 = long(input())
+	reg3 = long(input())
+	if reg3 > reg2:
+		reg1 = 0
+	else:
+		reg1 = reg2 - reg3
+# 	print('00001'+'00'+bin(reg1)+bin(reg2)+bin(reg3))
+	
 def main():
 	addition()
 
