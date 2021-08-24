52a64bee6c9abb6f95f922c04037a1b22741803d
Debjit Pramanik <79047521+debjit20504@users.noreply.github.com>
2021-08-24 19:24:32 +0530

    Update binary.py

--- Simple-Assembler/binary.py
+++ Simple-Assembler/binary.py
@@ -1,3 +1,4 @@
+import sys
 dic ={"add" : "00000", "sub" : "00001", "mov" : "00010",
 	  "mov" : "00011", "ld" : "00100", "st" :"00101", "mul" : "00110",
 	  "div" : "00111", "rs" : "01000", "ls" : "01001", "xor" : "01010",
@@ -50,7 +51,7 @@ def type_C(input_lst):
 		res += reg_dict[input_lst[1]]
 		res += reg_dict[input_lst[2]]
 		return res
-	else:	
+	else:    
 		for i in dic:
 			if input_lst[0] == i:
 				res = dic[i]
@@ -79,13 +80,14 @@ def type_F(input_lst):
 	res = '10011'
 	res += '00000000000'
 	return res
-	
+
 
 def main():
-	f = open(r"","r")
+	f = sys.stdin.read()
 	l = []
-	temp = 0
-	l = f.readlines()
+	temp = -1
+	count = ''
+	l = f.splitlines()
 	for i in l:
 		str = ''
 		for j in i:
@@ -94,7 +96,8 @@ def main():
 		temp += 1
 
 		if input_lst[0] == 'var':
-				pass
+			count = input_lst[1]
+			pass
 
 		if (len(input_lst) < 2 or len(input_lst) > 5) and input_lst[0] != 'var' and input_lst[0] != 'hlt':
 			print('ERROR', input_lst)
@@ -103,41 +106,51 @@ def main():
 				input_lst.remove(input_lst[0])
 
 			if input_lst[0] == 'add' or input_lst[0] == 'sub' or input_lst[0] == 'mul' or input_lst[0] == 'xor' or input_lst[0] == 'or' or input_lst[0] == 'and':
-				print(type_A(input_lst))
+				sys.stdout.write(type_A(input_lst) + '\n')
 
 			if input_lst[0] == 'mov':
 					if input_lst[2] in reg_dict:
-						print(type_C(input_lst))
+						sys.stdout.write(type_C(input_lst) + '\n')
 					else:
-						print(type_B(input_lst))
+						sys.stdout.write(type_B(input_lst) + '\n')
 
 			if input_lst[0] == 'rs' or input_lst[0] == 'ls':
-				print(type_B(input_lst))
+				sys.stdout.write(type_B(input_lst) + '\n')
 		
 			if input_lst[0] == 'div' or input_lst[0] == 'not' or input_lst[0] == 'cmp':
-				print(type_C(input_lst))
+				sys.stdout.write(type_C(input_lst) + '\n')
 		
 			if input_lst[0] == 'ld' or input_lst[0] == 'st':
 				if len(input_lst[2]) != 8:
-					x = int_convert(temp)
-					input_lst[2] = x
-					print(type_D(input_lst))
-				else:	
-					print(type_D(input_lst))
+					if count == input_lst[2]:
+						temp += 1
+						x = int_convert(temp)
+						input_lst[2] = x
+						sys.stdout.write(type_D(input_lst) + '\n')
+					else:
+						x = int_convert(temp)
+						input_lst[2] = x
+						sys.stdout.write(type_D(input_lst) + '\n')
+				else:    
+					sys.stdout.write(type_D(input_lst) + '\n')
 		
 			if input_lst[0] == 'jmp' or input_lst[0] == 'jlt' or input_lst[0] == 'jgt' or input_lst[0] == 'je':
 				if len(input_lst[1]) != 8:
-					x = int_convert(temp)
-					input_lst[1] = x
-					print(type_E(input_lst))
+					if count == input_lst[1]:
+						temp += 1
+						x = int_convert(temp)
+						input_lst[1] = x
+						sys.stdout.write(type_E(input_lst) + '\n')
+					else:
+						x = int_convert(temp)
+						input_lst[1] = x
+						sys.stdout.write(type_E(input_lst) + '\n')
 				else:
-					print(type_E(input_lst))
+					sys.stdout.write(type_E(input_lst) + '\n')
 		
 			if input_lst[0] == 'hlt':
-				print(type_F(input_lst))
+				sys.stdout.write(type_F(input_lst) + '\n')
 				
 
 if __name__ == '__main__':
 	main()
-
-
